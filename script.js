3d57458ff573f76945108a9cd133a1fae5a6a99d
sugimoto07 <35365866+sugimoto07@users.noreply.github.com>
2021-02-03 22:23:30 -0800

    First version of mixed messages program

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-console.log("Testing");
+messages = {
+    firstPartOptions: ["Good News!","Bad News.","Tough News.","Awesome News!","Got some news.","Hello!"],
+    secondPartOptions: ["Tomorrow","Later today","Tonight","Very soon","At some point","In a fortnight","Within the week"],
+    thirdPartOptions: ["you will win $1 million dollars!","you will discover tragedy.","you will meet your maker.","something shiny will come your way.","you will set off on an adventure.","you will have incredible luck!"],
 
+    generateMessage() {
+        let options = this.firstPartOptions;
+        let random = Math.floor(Math.random() * options.length);
+        let message = options[random];
 
-console.log("adding a 4th line")
-console.log("Testing again")
+        options = this.secondPartOptions;
+        random = Math.floor(Math.random() * options.length);
+        message = message + " " + options[random];
 
-console.log("Testing Terminal")
+        options = this.thirdPartOptions;
+        random = Math.floor(Math.random() * options.length);
+        message = message + " " + options[random];
+
+        return message;
+    }
+}
+
+console.log(messages.generateMessage());
